9efee5329e52fb7996feabbcad55ab832342f354
Miguel <miguel.almonte1@gmail.com>
2017-12-28 16:52:09 -0400

    Add CRUD schema of dish to graphql endpoint

--- dishes/schema.py
+++ dishes/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from .models import Category
+from .models import Category, Dish
 
 
 class CategoryType(DjangoObjectType):
@@ -73,12 +73,91 @@ class DeleteCategory(graphene.Mutation):
             return DeleteCategory(category=None, ok=ok)
 
 
+class DishType(DjangoObjectType):
+    class Meta:
+        model = Dish
+
+
+class CreateDish(graphene.Mutation):
+    class Arguments:
+        category_id = graphene.String()
+        name = graphene.String()
+        description = graphene.String()
+        price = graphene.Float()
+
+    ok = graphene.Boolean()
+    dish = graphene.Field(lambda: DishType)
+
+    def mutate(self, info, category_id, name, description="", price=0):
+        if name and name:
+            new_dish = Dish.objects.create(category_id=category_id, name=name, description=description, price=price)
+        else:
+            ok = False
+            return CreateDish(dish=None, ok=ok)
+        dish = DishType(name=name, description=description, price=price)
+        ok = True
+        return CreateDish(dish=dish, ok=ok)
+
+
+class EditDish(graphene.Mutation):
+    class Arguments:
+        id = graphene.String()
+        category_id = graphene.String()
+        name = graphene.String()
+        description = graphene.String()
+        price = graphene.Float()
+    ok = graphene.Boolean()
+    dish = graphene.Field(lambda: DishType)
+
+    def mutate(self, info, id, category_id=None, name=None, description=None, price=None):
+        if id:
+            d = Dish.objects.get(id=id)
+            if name:
+                d.name = name
+            if description:
+                d.description = description
+            if price is not None:
+                d.price = price
+            if category_id:
+                d.category_id = category_id
+            d.save()
+            dish = DishType(id=d.pk, name=d.name, description=d.description, price=d.price)
+            ok = True
+            return EditDish(dish=dish, ok=ok)
+        else:
+            ok = False
+            return EditDish(dish=None, ok=ok)
+
+
+class DeleteDish(graphene.Mutation):
+    class Arguments:
+        id = graphene.String()
+
+    ok = graphene.Boolean()
+    dish = graphene.Field(lambda: DishType)
+
+    def mutate(self, info, id):
+        if id:
+            d = Dish.objects.get(id=id)
+            d.delete()
+            di = DishType(name=d.name, description=d.description, price=d.price)
+            ok = True
+            return DeleteDish(dish=di, ok=ok)
+        else:
+            ok = False
+            return DeleteDish(category=None, ok=ok)
+
+
 class Query:
     all_categories = graphene.List(CategoryType)
-
     category = graphene.Field(CategoryType,
                               id=graphene.Int(),
                               name=graphene.String())
+    all_dishes = graphene.List(DishType)
+
+    dish = graphene.Field(DishType,
+                          id=graphene.Int(),
+                          name=graphene.String())
 
     def resolve_all_categories(self, info, **kwargs):
         return Category.objects.all()
@@ -92,3 +171,16 @@ class Query:
 
         if name is not None:
             return Category.objects.get(name=name)
+
+    def resolve_all_diches(self, info, **kwargs):
+        return Dish.objects.select_related('category').all()
+
+    def resolve_dish(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+
+        if id is not None:
+            return Dish.objects.get(pk=id)
+
+        if name is not None:
+            return Dish.objects.get(name=name)
--- restaurant/schema.py
+++ restaurant/schema.py
@@ -10,6 +10,9 @@ class Mutations(graphene.ObjectType):
     create_category = dishes.schema.CreateCategory.Field()
     edit_category = dishes.schema.EditCategory.Field()
     delete_category = dishes.schema.DeleteCategory.Field()
+    create_dish = dishes.schema.CreateDish.Field()
+    edit_dish = dishes.schema.EditDish.Field()
+    delete_dish = dishes.schema.DeleteDish.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutations)
