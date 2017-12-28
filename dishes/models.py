9996c2f595f652813bf1db0dad0b7cec70308722
Miguel <miguel.almonte1@gmail.com>
2017-12-28 16:51:36 -0400

    Create dish model and migrations

--- dishes/models.py
+++ dishes/models.py
@@ -8,3 +8,12 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+class Dish(models.Model):
+    name = models.CharField(max_length=150)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    description = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
